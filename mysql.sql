e25e177ecc81c138b1ad760eb7cfe4b703050c6b
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2015-06-30 23:33:23 +0200

    date_added qutoa table cant be CURRENT_TIMESTAMP. App must ensure everything is in UTC

--- mysql.sql
+++ mysql.sql
@@ -84,7 +84,7 @@ CREATE TABLE quota (
   is_regex tinyint(1) DEFAULT '0',
   profile bigint(20) unsigned NOT NULL,
   instigator bigint(20) unsigned DEFAULT NULL,
-  date_added timestamp DEFAULT CURRENT_TIMESTAMP,
+  date_added datetime NOT NULL,
   PRIMARY KEY id (id),
   UNIQUE KEY selector (selector,value,profile),
   KEY profile (profile),
