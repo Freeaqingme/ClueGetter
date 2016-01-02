61f008912b96ee4cfb21465c723c847fecbba246
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2016-01-02 01:40:18 +0100

    Time flies! s/2015/2016/

--- src/cluegetter/util.go
+++ src/cluegetter/util.go
@@ -1,6 +1,6 @@
 // ClueGetter - Does things with mail
 //
-// Copyright 2015 Dolf Schimmel, Freeaqingme.
+// Copyright 2016 Dolf Schimmel, Freeaqingme.
 //
 // This Source Code Form is subject to the terms of the two-clause BSD license.
 // For its contents, please refer to the LICENSE file.
