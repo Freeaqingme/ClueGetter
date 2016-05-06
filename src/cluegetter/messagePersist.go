e4e13ff49bde7e24a805de7f65564fc2a117c67a
Dolf Schimmel (Freeaqingme) <dolf@dolfschimmel.nl>
2016-05-06 23:00:30 +0200

    Use allegro/bigcache for in-memory message cache

--- src/cluegetter/messagePersist.go
+++ src/cluegetter/messagePersist.go
@@ -7,9 +7,9 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"sync"
-	"sync/atomic"
 	"time"
+
+	"github.com/allegro/bigcache"
 )
 
 var (
@@ -44,13 +44,6 @@ func messagePersistStart() {
 	} else {
 		Log.Info("archive-prune-interval set to 0. Not pruning anything.")
 	}
-
-	ticker := time.NewTicker(time.Second * 30)
-	go func() {
-		for range ticker.C {
-			messagePersistCache.prune()
-		}
-	}()
 }
 
 func messagePersistHandleQueue(queue chan []byte) {
@@ -427,45 +420,58 @@ func messagePersistInCache(queueId string, msgId string, msg []byte) {
 // Message Cache //
 ///////////////////
 
-var messagePersistCache = messageCacheNew(5*1024*1024, 512*1024*1024)
+var messagePersistCache = messageCacheNew(5 * 1024 * 1024)
 
 type messageCache struct {
-	sync.RWMutex
-	cache        map[string][]byte
-	age          map[string]int64
-	msgIdIdx     map[string]string
-	msgIdRevIdx  map[string]string
-	size         uint64
-	maxSize      uint64
+	bigcache     *bigcache.BigCache
+	msgIdIdx     *bigcache.BigCache
 	maxEntrySize uint64
 }
 
-func messageCacheNew(maxEntrySize uint64, maxSize uint64) *messageCache {
+func messageCacheNew(maxEntrySize uint64) *messageCache {
+	config := bigcache.Config{
+		Shards:             1024,
+		LifeWindow:         10 * time.Minute,
+		MaxEntriesInWindow: 1000 * 10 * 60,
+		MaxEntrySize:       500,
+		Verbose:            true,
+		HardMaxCacheSize:   1024, // Value in MB
+	}
+
+	cache, initErr := bigcache.NewBigCache(config)
+	if initErr != nil {
+		Log.Fatal(initErr)
+	}
+
+	msgIdIdx, initErr := bigcache.NewBigCache(config)
+	if initErr != nil {
+		Log.Fatal(initErr)
+	}
+
 	return &messageCache{
-		cache:        make(map[string][]byte),
-		age:          make(map[string]int64),
-		msgIdIdx:     make(map[string]string),
-		msgIdRevIdx:  make(map[string]string),
-		size:         0,
-		maxSize:      maxSize,
+		bigcache:     cache,
+		msgIdIdx:     msgIdIdx,
 		maxEntrySize: maxEntrySize,
 	}
 }
 
 func (c *messageCache) getByQueueId(queueId string) []byte {
-	c.RLock()
-	defer c.RUnlock()
+	res, err := c.bigcache.Get(queueId)
+	if err != nil {
+		Log.Error(err.Error())
+		return []byte{}
+	}
 
-	return c.cache[queueId]
+	return res
 }
 
 func (c *messageCache) getByMessageId(msgId string) []byte {
-	c.RLock()
-	queueId := c.msgIdIdx[msgId]
-	protoBuf := c.cache[queueId]
-	c.RUnlock()
-
-	return protoBuf
+	queueId, err := c.msgIdIdx.Get(msgId)
+	if err != nil {
+		Log.Error(err.Error())
+		return []byte{}
+	}
+	return c.getByQueueId(string(queueId))
 }
 
 func (c *messageCache) Set(queueId, msgId string, msg []byte) (bool, error) {
@@ -477,74 +483,8 @@ func (c *messageCache) Set(queueId, msgId string, msg []byte) (bool, error) {
 		))
 	}
 
-	c.Lock()
-	defer c.Unlock()
-
-	if c.size+size > c.maxSize {
-		return false, errors.New(fmt.Sprintf(
-			"Could not cache item '%s' (size %d). Total cache size would be exceeded.",
-			queueId, size,
-		))
-	}
-
-	if _, exists := c.cache[queueId]; exists {
-		c._del(queueId)
-	}
-
-	c.msgIdIdx[msgId] = queueId
-	c.msgIdRevIdx[queueId] = msgId
-
-	c.cache[queueId] = msg
-	c.age[queueId] = time.Now().Unix()
-	atomic.AddUint64(&c.size, size)
+	c.msgIdIdx.Set(msgId, []byte(queueId))
+	c.bigcache.Set(queueId, msg)
 
 	return true, nil
-}
-
-func (c *messageCache) _del(id string) {
-	item, exists := c.cache[id]
-	if !exists {
-		return
-	}
-
-	size := len(item)
-	delete(c.cache, id)
-	delete(c.age, id)
-	atomic.AddUint64(&c.size, ^uint64(size-1))
-
-	delete(c.msgIdIdx, c.msgIdRevIdx[id])
-	delete(c.msgIdRevIdx, id)
-}
-
-func (c *messageCache) prune() {
-	if c.size < c.maxSize/2 {
-		Log.Debug("Skipping pruning of messageCache it's below 50%% (%d/%d) capacity.", c.size, c.maxSize)
-		return
-	}
-
-	t0 := time.Now()
-	cutoff := t0.Unix() - 300
-	prune := make([]string, 0)
-	c.RLock()
-	for key, age := range c.age {
-		if age < cutoff {
-			prune = append(prune, key)
-		}
-	}
-	c.RUnlock()
-	Log.Debug("Scanned for prunable message cache items in %s", time.Now().Sub(t0).String())
-	t0 = time.Now()
-	if len(prune) == 0 {
-		goto end
-	}
-
-	c.Lock()
-	defer c.Unlock()
-
-	for _, key := range prune {
-		c._del(key)
-	}
-end:
-	Log.Debug("Pruned %d message cache items in %s. It now contains %d items, %d bytes",
-		len(prune), time.Now().Sub(t0).String(), len(c.cache), c.size)
 }
